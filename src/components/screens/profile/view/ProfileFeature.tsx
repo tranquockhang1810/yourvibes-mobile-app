33859edeeda8e55617277b703472cc824ee12f83
MTPE <mtpe2301@gmail.com>
2024-10-12 12:08:37 +0700

    ui profile

--- src/components/screens/profile/view/ProfileFeature.tsx
+++ src/components/screens/profile/view/ProfileFeature.tsx
@@ -8,71 +8,70 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Platform,
-  Alert, 
+  Alert,
 } from 'react-native';
 import { Tabs, Button } from '@ant-design/react-native';
 import { Feather, MaterialIcons } from '@expo/vector-icons';
 import { useAuth } from '@/src/context/useAuth';
-import useColor from '@/src/hooks/useColor';  
+import useColor from '@/src/hooks/useColor';
+// import { ProfileViewModel } from '../viewModel/ProfileViewModel';
 
 const ProfileScreen = () => {
   const { brandPrimary, brandPrimaryTap, backgroundColor, lightGray } = useColor();
-  const { onLogout, changeLanguage, localStrings } = useAuth(); 
- 
-const handleLogout = () => {
+  const { onLogout, changeLanguage, localStrings } = useAuth();
+  // const { handleLogout} = ProfileViewModel();
+  const handleLogout = () => {
     Alert.alert(
       `Xác nhận`,
       `Bạn có chắc chắn muốn đăng xuất không?`,
       [
         { text: 'Hủy', style: 'cancel' },
-        { text: 'Đăng xuất', onPress: onLogout }
+        { text: 'Đăng xuất', onPress: onLogout },
       ]
     );
   };
 
-const friends = Array.from({ length: 8 }, (_, index) => `Bạn bè ${index + 1}`);  
-
+  const friends = Array.from({ length: 8 }, (_, index) => `Bạn bè ${index + 1}`);
 
   return (
     <KeyboardAvoidingView
       style={{ flex: 1, backgroundColor: backgroundColor, width: '100%' }}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
     >
-      <SafeAreaView style={{ flex: 2 }}>
-      <ScrollView
-        style={{ width: '100%' }}
-        contentContainerStyle={{ flexGrow: 1, alignItems: 'center', paddingBottom: 20 }}
-      >
-
+      <SafeAreaView style={{ flex: 1 }}>
         {/* Header */}
         <View
-            style={{
-                marginTop: 40,
-                width: '100%',
-                paddingHorizontal: 16,
-                paddingTop: 16,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-            }}
-          >
-            <TouchableOpacity>
-              <Feather name="arrow-left" size={24} color="black" />
-            </TouchableOpacity>
-            <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: 'bold', flex: 1 }}>
-              Họ tên đầy đủ của người đăng nhập
-            </Text>
-          </View>
-          
-          {/* Black Line Divider */}
-          <View
-            style={{
-                width: '100%',
-                height: 1, 
-                backgroundColor: brandPrimary, 
-            }}
-          />
+          style={{
+            marginTop: 40,
+            width: '100%',
+            paddingHorizontal: 16,
+            paddingTop: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <TouchableOpacity>
+            <Feather name="arrow-left" size={24} color="black" />
+          </TouchableOpacity>
+          <Text style={{ textAlign: 'center', fontSize: 18, fontWeight: 'bold', flex: 1 }}>
+            Họ tên đầy đủ của người đăng nhập
+          </Text>
+        </View>
 
+        {/* Black Line Divider */}
+        <View
+          style={{
+            width: '100%',
+            height: 1,
+            backgroundColor: brandPrimary,
+          }}
+        />
+
+        <ScrollView
+          style={{ width: '100%' }}
+          contentContainerStyle={{ flexGrow: 1, alignItems: 'center', paddingBottom: 20 }}
+        >
           {/* Cover Image */}
           <View style={{ width: '100%', height: 150, backgroundColor: lightGray }}>
             <Image
@@ -125,23 +124,21 @@ const friends = Array.from({ length: 8 }, (_, index) => `Bạn bè ${index + 1}`
             </View>
           </View>
 
-            {/* Tabs Section */}
-            <View style={{ flex: 1, marginTop: 20 }}>
-              <Tabs
-                tabs={[
-                  { title: 'Bài viết' },
-                  { title: 'Giới thiệu' },
-                  { title: 'Cài đặt trang cá nhân' },
-                ]}
-                initialPage={0}
-                tabBarPosition="top"
-              >
-                {/* Bài viết */}
-                <ScrollView
-                  style={{ padding: 16 }} 
-                >
-                  {/* Post Input Section */}
-                  <View
+          {/* Tabs Section */}
+          <View style={{ flex: 1, marginTop: 20 }}>
+            <Tabs
+              tabs={[
+                { title: 'Bài viết' },
+                { title: 'Giới thiệu' },
+                { title: 'Cài đặt trang cá nhân' },
+              ]}
+              initialPage={0}
+              tabBarPosition="top"
+            >
+              {/* Bài viết */}
+              <ScrollView style={{ padding: 16 }}> 
+                 {/* Post Input Section */}
+                 <View
                     style={{
                       width: '100%',
                       padding: 16,
@@ -165,74 +162,76 @@ const friends = Array.from({ length: 8 }, (_, index) => `Bạn bè ${index + 1}`
                   </View>
 
                   {/* Nội dung bài viết */}
-                  <Text style={{ padding: 20, fontSize: 18, fontWeight: 'bold' }}>Các bài viết hiển thị ở đây</Text>
-                </ScrollView>
+                  <Text style={{ padding: 20, fontSize: 18, fontWeight: 'bold' }}>Các bài viết hiển thị ở đây</Text> 
+              </ScrollView>
 
-              {/* Giới thiệu */} 
+              {/* Giới thiệu */}
               <View style={{ padding: 20, flex: 1 }}>
                 <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>Chi tiết</Text>
+                  
+                <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>Chi tiết</Text>
 
-                  {/* Email */}
-                  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                    <MaterialIcons name="email" size={24} color="black" />
-                    <Text style={{ marginLeft: 10 }}>Email: <Text style={{ fontWeight: 'bold' }}>Sample@test.com</Text></Text>
-                  </View>
+                {/* Email */}
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                  <MaterialIcons name="email" size={24} color="black" />
+                  <Text style={{ marginLeft: 10 }}>Email: <Text style={{ fontWeight: 'bold' }}>Sample@test.com</Text></Text>
+                </View>
 
-                  {/* Số điện thoại */}
-                  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                    <Feather name="phone" size={24} color="black" />
-                    <Text style={{ marginLeft: 10 }}>Số điện thoại: <Text style={{ fontWeight: 'bold' }}>0912345678</Text></Text>
-                  </View>
+                {/* Số điện thoại */}
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                  <Feather name="phone" size={24} color="black" />
+                  <Text style={{ marginLeft: 10 }}>Số điện thoại: <Text style={{ fontWeight: 'bold' }}>0912345678</Text></Text>
+                </View>
 
-                  {/* Ngày sinh */}
-                  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                    <Feather name="calendar" size={24} color="black" />
-                    <Text style={{ marginLeft: 10 }}>Ngày sinh: <Text style={{ fontWeight: 'bold' }}>23/01/2000</Text></Text>
-                  </View>
+                {/* Ngày sinh */}
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                  <Feather name="calendar" size={24} color="black" />
+                  <Text style={{ marginLeft: 10 }}>Ngày sinh: <Text style={{ fontWeight: 'bold' }}>23/01/2000</Text></Text>
+                </View>
 
-                  {/* Ngày hoạt động */}
-                  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
-                    <MaterialIcons name="date-range" size={24} color="black" />
-                    <Text style={{ marginLeft: 10 }}>Ngày hoạt động: <Text style={{ fontWeight: 'bold' }}>20/09/2024</Text></Text>
-                  </View>
+                {/* Ngày hoạt động */}
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                  <MaterialIcons name="date-range" size={24} color="black" />
+                  <Text style={{ marginLeft: 10 }}>Ngày hoạt động: <Text style={{ fontWeight: 'bold' }}>20/09/2024</Text></Text>
+                </View>
 
-                  {/* Danh sách bạn bè */}
-                  <View style={{ padding: 20 }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 }}>
-                      <View>
-                        <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Bạn bè</Text>
-                        <Text>435 người bạn</Text>
-                      </View> 
-                      <TouchableOpacity onPress={() => { /* Xử lý tìm bạn bè */ }}>
-                        <Text style={{ alignSelf: 'flex-end' , color: 'blue' }}>Tìm bạn bè</Text>
-                      </TouchableOpacity>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-                      {friends.map((friend, index) => (
-                        <View key={index} style={{ width: '23%', alignItems: 'center', marginBottom: 10 }}>
-                          <View style={{
-                            width: 60, height: 60, borderRadius: 30, backgroundColor: lightGray,
-                            justifyContent: 'center', alignItems: 'center'
-                          }}> 
-                          </View>
-                          <Text style={{ marginTop: 5 }}>{friend}</Text>
-                        </View>
-                      ))}
-                    </View>
-
-                    <TouchableOpacity onPress={() => { /* Xử lý xem tất cả bạn bè */ }}>
-                      <Text style={{ textAlign: 'center', marginTop: 20, color: 'blue' }}>Xem tất cả bạn bè</Text>
+                {/* Danh sách bạn bè */}
+                <View style={{ padding: 20 }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20 }}>
+                    <View>
+                      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Bạn bè</Text>
+                      <Text>435 người bạn</Text>
+                    </View> 
+                    <TouchableOpacity onPress={() => { /* Xử lý tìm bạn bè */ }}>
+                      <Text style={{ alignSelf: 'flex-end' , color: 'blue' }}>Tìm bạn bè</Text>
                     </TouchableOpacity>
                   </View>
-                </ScrollView>
-              </View> 
 
-                {/* Cài đặt trang cá nhân */}
-                <View style={{ padding: 20, flex: 1 }}>
-                  <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}> 
-                  <Button
+                  <View style={{ flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+                    {friends.map((friend, index) => (
+                      <View key={index} style={{ width: '23%', alignItems: 'center', marginBottom: 10 }}>
+                        <View style={{
+                          width: 60, height: 60, borderRadius: 30, backgroundColor: lightGray,
+                          justifyContent: 'center', alignItems: 'center'
+                        }}> 
+                        </View>
+                        <Text style={{ marginTop: 5 }}>{friend}</Text>
+                      </View>
+                    ))}
+                  </View>
+
+                  <TouchableOpacity onPress={() => { /* Xử lý xem tất cả bạn bè */ }}>
+                    <Text style={{ textAlign: 'center', marginTop: 20, color: 'blue' }}>Xem tất cả bạn bè</Text>
+                  </TouchableOpacity>
+                </View>
+
+                </ScrollView>
+              </View>
+
+              {/* Cài đặt trang cá nhân */}
+              <View style={{ padding: 20, flex: 1 }}>
+                <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+                <Button
                     type="primary"
                     onPress={() => {
                       // Handle Edit Profile action here
@@ -251,17 +250,16 @@ const friends = Array.from({ length: 8 }, (_, index) => `Bạn bè ${index + 1}`
                   </Button>
                   <Button
                     type="primary"
-                    onPress={handleLogout}
-                  >
-                    Đăng xuất
-                  </Button>
-                  <Button
-                    type="primary"
                     onPress={() => changeLanguage()}
-                    style={{ marginTop: 10 }}
                   >
                     {localStrings.Public.Language}
                   </Button>
+
+                  <Button type="primary" onPress={handleLogout} 
+                    style={{ marginTop: 10 }}>
+                    Đăng xuất
+                  </Button>
+
                 </ScrollView>
               </View>
             </Tabs>
--- src/components/screens/profile/viewModel/ProfileViewModel.ts
+++ src/components/screens/profile/viewModel/ProfileViewModel.ts
@@ -1,13 +1,21 @@
 import { Alert } from 'react-native';
 
-export const handleLogout = (onLogout: () => void) => {
-  Alert.alert(
-    `Xác nhận`,
-    `Bạn có chắc chắn muốn đăng xuất không?`,
-    [
-      { text: 'Hủy', style: 'cancel' },
-      { text: 'Đăng xuất', onPress: onLogout },
-    ]
-  );
-};
+const ProfileViewModel = () => {
 
+  const handleLogout = () => {
+    Alert.alert( 
+      'Xác nhận',
+      'µn có chắc chắn muốn đăng xuất',
+      [
+        { text: 'Hủy', style: 'cancel' },
+        { text: 'Đăng xuất', onPress: () => console.log('Đăng xuất') },
+      ]
+    );
+  };
+
+  return {
+    handleLogout
+  }
+}
+
+export default ProfileViewModel
